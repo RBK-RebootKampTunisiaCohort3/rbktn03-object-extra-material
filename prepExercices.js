d1f4847d340a2dcc49bf1b838f92afcd28870905
CharafChroudi <charafchroudi17@gmail.com>
2020-03-27 12:03:15 +0100

    Solved question 6,7,8,9,10 and 11

--- prepExercices.js
+++ prepExercices.js
@@ -1,15 +1,23 @@
 // Exercises
 // Basic Requirments
 // 1.Fix the syntax & style issues with the three objects below:
-{firstName: 'Yan', lastname: 'Fan' }
-{a: 1, b:2, c: 3, d: 4}
-{
-       animal: 'dog',
-       noise: 'bark',
-       age: 3,
-       type: 'Golden Retriever',
-       color: 'Yellow'
- }
+var person = {
+	firstName: 'Yan',
+  lastname: 'Fan'
+};
+var numbers = {
+	a: 1,
+ 	b:2,
+ 	c: 3,
+  d: 4
+};
+var puppy = {
+	 animal: 'dog',
+	 noise: 'bark',
+	 age: 3,
+	 type: 'Golden Retriever',
+	 color: 'Yellow'
+ };
 
 // 2. Create an object that represents you. It should contain your first name, last name, age and hometown. Assign it to a variable called person.
 var person = {
@@ -49,42 +57,53 @@ var shoes = {
 	price: '279$'
 }
 // 6.in the console,Create a new empty object in your console called obj like this:
-// var obj = {};
+var obj = {};
 
 // 7.Add a new key/value pair to the object obj by assigning a new value to a new key like so:
-//  obj.hello = 'world';
-//  obj['number'] = 25;
+obj.hello = 'world';
+obj['number'] = 25;
 
 // 8. Now, check the value of obj in the console and ensure that it has the two key/value pairs added above. This is how we create new key/value pairs in existing objects.
-
+obj
+{hello: "world", number: 25}
 // 9.In the console attached to your main.js file, add a favoriteColor key/value pair to the object that represents you.
-
+person.favoriteColor = 'Blue';
 // 10.Fix the attempts to access values in the person object:
-//  var key = 'name';
-//  var woman = {
-//        name: 'Grace Hopper',
-//        age: 85,
-//        occupation: 'computer scientist'
-//  };
-//  woman[age]; // => 85
-//  woman.key; // => 'Grace Hopper'
-//  woman['computer scientist'] // => ???
+var key = 'name';
+var woman = {
+     name: 'Grace Hopper',
+     age: 85,
+     occupation: 'computer scientist'
+};
+woman["age"] // => 85
+woman[key] // => "Grace Hopper"
+woman['occupation'] // => "computer scientist"
 //  Side Note: Who is Grace Hopper? She is one of the most influential people in the history of computer science and software engineering.
 //  Read more about her contribution to our field here. (https://en.wikipedia.org/wiki/Grace_Hopper)
 
 // 11.Write a function formatName that takes the person object that you created above (the first exercise at the top) as an argument, and returns your full name.
-
+function formatName(person){
+	return person.fullName;
+}
 // 12.Using these objects:
-//  var people = [
-//        {name: {first: 'Grace', middle: 'B.', last: 'Hopper'}, age: 85},
-//        {name: {first: 'Adele', last: 'Goldstine'}, age: 43},
-//        {name: {first: 'Ada', last: 'Lovelace'}, age: 36},
-//        {name: {first: 'Hedy', middle: 'E.', last: 'Lamarr'}, age: 85},
-//        {name: {first: 'Ruchi', last: 'Sanghvi'}, age: 34}
-//  ];
+var people = [
+     {name: {first: 'Grace', middle: 'B.', last: 'Hopper'}, age: 85},
+     {name: {first: 'Adele', last: 'Goldstine'}, age: 43},
+     {name: {first: 'Ada', last: 'Lovelace'}, age: 36},
+     {name: {first: 'Hedy', middle: 'E.', last: 'Lamarr'}, age: 85},
+     {name: {first: 'Ruchi', last: 'Sanghvi'}, age: 34},
+     {name: {first: 'Charaf',last: 'Chroudi'}, age: 23}
+];
 //  Add the object representing yourself (person) to this array of people (if your name key does not have the same 'shape' as the ones above,
 //  change it to look like these).
 //  Write a function that, when passed people as an argument, returns an array of their full names. Can you use your formatName function here?
+function returnName(people){
+	var arrayOfNames = [];
+	for (var i = 0; i < people.length; i++){
+		arrayOfNames.push(people[i]);
+	}
+	return arrayOfNames;
+}
 //  Write a function that finds the average age of the people array.
 //  Write a function that, when given people and an age as arguments, returns an array of just the people that are older than the specified age.
 //  Side Note: The women in the people array are all very famous. What did they do?
